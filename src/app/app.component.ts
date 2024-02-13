0c0f747a0db319ecab0a5280661d37038ebce38b
Leonel <leonel.bruzzese@gmail.com>
2024-02-13 14:45:54 -0300

    agregando galeria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,22 @@ export class AppComponent {
 
   modal: boolean = false;
 
+  componenteActual: string = 'app-home';
+
+  opciones: any[] = [
+    { nombre: 'Home', componente: 'app-home' },
+    { nombre: 'galeria', componente: 'app-galeria' },
+    // Agrega más opciones según sea necesario
+  ];
+
+  cambiarComponente(componente: string) {
+    this.componenteActual = componente;
+    this.modal = false; // Oculta el menú después de hacer clic en una opción
+
+    const iconoCerrar = document.getElementById("button") as HTMLElement;
+    iconoCerrar.classList.toggle("cruz-cerrar");
+  }
+
   abrirMenu() {
     this.modal=!this.modal;
     
